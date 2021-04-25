619cccb0615209623655aeb498f429fd2798c44c
kaiqueiwata <kaiqueiwata2@hotmail.com>
2021-04-25 16:23:16 -0300

    ajuste mensagens exibidas

--- examples/viewer6/openglwindow.cpp
+++ examples/viewer6/openglwindow.cpp
@@ -33,24 +33,18 @@ void OpenGLWindow::handleEvent(SDL_Event& event) {
   }
   if (event.type == SDL_KEYDOWN) {
     // Ir pra frente e pra tras
-    if (event.key.keysym.sym == SDLK_w)
-      m_dollySpeed = 1.0f;
-    if (event.key.keysym.sym == SDLK_s)
-      m_dollySpeed = -1.0f;
+    if (event.key.keysym.sym == SDLK_w) m_dollySpeed = 1.0f;
+    if (event.key.keysym.sym == SDLK_s) m_dollySpeed = -1.0f;
 
     // Olhar pra direita e pra esquerda
-    if (event.key.keysym.sym == SDLK_LEFT)
-      m_panSpeed = -1.0f;
-    if (event.key.keysym.sym == SDLK_RIGHT)
-      m_panSpeed = 1.0f;  
+    if (event.key.keysym.sym == SDLK_LEFT) m_panSpeed = -1.0f;
+    if (event.key.keysym.sym == SDLK_RIGHT) m_panSpeed = 1.0f;
 
-    //Andar de um lado para outro
-    if (event.key.keysym.sym == SDLK_a) 
-      m_truckSpeed = -1.0f;
-    if (event.key.keysym.sym == SDLK_d) 
-      m_truckSpeed = 1.0f;
+    // Andar de um lado para outro
+    if (event.key.keysym.sym == SDLK_a) m_truckSpeed = -1.0f;
+    if (event.key.keysym.sym == SDLK_d) m_truckSpeed = 1.0f;
 
-    if(event.key.keysym.sym == SDLK_SPACE){
+    if (event.key.keysym.sym == SDLK_SPACE) {
       printf("Capturou a tecla (DOWN)\n");
       // if(!isJumping){
       //   isJumping = true;
@@ -58,29 +52,23 @@ void OpenGLWindow::handleEvent(SDL_Event& event) {
       // }
     }
   }
-  
+
   if (event.type == SDL_KEYUP) {
     // Parar movimento de ir pra frente e pra tras
-    if (event.key.keysym.sym == SDLK_w && m_dollySpeed > 0)
-      m_dollySpeed = 0.0f;
-    if ( event.key.keysym.sym == SDLK_s && m_dollySpeed < 0)
-      m_dollySpeed = 0.0f;
+    if (event.key.keysym.sym == SDLK_w && m_dollySpeed > 0) m_dollySpeed = 0.0f;
+    if (event.key.keysym.sym == SDLK_s && m_dollySpeed < 0) m_dollySpeed = 0.0f;
 
     // Parar de virar a camera de lado
-    if (event.key.keysym.sym == SDLK_LEFT && m_panSpeed < 0)
-      m_panSpeed = 0.0f;
-    if (event.key.keysym.sym == SDLK_RIGHT && m_panSpeed > 0)
-      m_panSpeed = 0.0f;
-    
-    //Parar de moviementar para os lados
-    if (event.key.keysym.sym == SDLK_a && m_truckSpeed < 0) 
-      m_truckSpeed = 0.0f;
-    if (event.key.keysym.sym == SDLK_d && m_truckSpeed > 0) 
-      m_truckSpeed = 0.0f;
+    if (event.key.keysym.sym == SDLK_LEFT && m_panSpeed < 0) m_panSpeed = 0.0f;
+    if (event.key.keysym.sym == SDLK_RIGHT && m_panSpeed > 0) m_panSpeed = 0.0f;
 
-    if(event.key.keysym.sym == SDLK_SPACE){
+    // Parar de moviementar para os lados
+    if (event.key.keysym.sym == SDLK_a && m_truckSpeed < 0) m_truckSpeed = 0.0f;
+    if (event.key.keysym.sym == SDLK_d && m_truckSpeed > 0) m_truckSpeed = 0.0f;
+
+    if (event.key.keysym.sym == SDLK_SPACE) {
       printf("Capturou a tecla (UP)\n");
-      if(!isJumping){
+      if (!isJumping) {
         isJumping = true;
         m_jumpSpeed = 2.0f;
       }
@@ -92,31 +80,32 @@ void OpenGLWindow::initializeGL() {
   glClearColor(0, 0, 0, 1);
   glEnable(GL_DEPTH_TEST);
 
-  ImGuiIO &io{ImGui::GetIO()};
+  ImGuiIO& io{ImGui::GetIO()};
 
   houveColisao = false;
-  pontos = 0; 
+  pontos = 0;
 
-
-  //configuracoes de fontes
+  // configuracoes de fontes
   {
-    //Fontes do titulo e mensagem de gameover/titulo
+    // Fontes do titulo e mensagem de gameover/titulo
     auto ttf_gameover{getAssetsPath() + "fonts/HardsignLayered-eZ1MB.ttf"};
-    auto ttf_message{getAssetsPath() + "fonts/PlayfairDisplaySemibold-lg9nd.ttf"};
+    auto ttf_message{getAssetsPath() +
+                     "fonts/PlayfairDisplaySemibold-lg9nd.ttf"};
 
-    //Titulo e GameOver sao as mesmas com tamanho diferente
-    m_font_game_over = io.Fonts->AddFontFromFileTTF(ttf_gameover.c_str(), 45.0f);
+    // Titulo e GameOver sao as mesmas com tamanho diferente
+    m_font_game_over =
+        io.Fonts->AddFontFromFileTTF(ttf_gameover.c_str(), 45.0f);
     m_font_title = io.Fonts->AddFontFromFileTTF(ttf_gameover.c_str(), 65.0f);
 
-    //A fonte de mensagem, usada pra mostrar os pontos e avisar 
-    //e que o jogo ta acelerando, eh uma fonte mais simples
+    // A fonte de mensagem, usada pra mostrar os pontos e avisar
+    // e que o jogo ta acelerando, eh uma fonte mais simples
     m_font_message = io.Fonts->AddFontFromFileTTF(ttf_message.c_str(), 25.0f);
-    
-    if (m_font_game_over == nullptr || m_font_message == nullptr || m_font_title == nullptr) {
+
+    if (m_font_game_over == nullptr || m_font_message == nullptr ||
+        m_font_title == nullptr) {
       throw abcg::Exception{abcg::Exception::Runtime("Cannot load font file")};
     }
   }
-
 
   // Create programs
   for (const auto& name : m_shaderNames) {
@@ -134,7 +123,7 @@ void OpenGLWindow::initializeGL() {
   // Initial trackball spin
   m_trackBallModel.setAxis(glm::normalize(glm::vec3(1, 1, 1)));
 
-  //Aqui esta definindo a posicao inicial do tronco 
+  // Aqui esta definindo a posicao inicial do tronco
   m_modelMatrix = glm::translate(m_modelMatrix, glm::vec3(0, 0, -1));
 
   initializeSkybox();
@@ -142,7 +131,7 @@ void OpenGLWindow::initializeGL() {
 
 void OpenGLWindow::initializeSkybox() {
   // Create skybox program
-  auto path{getAssetsPath() + "shaders/" + m_skyShaderName};  
+  auto path{getAssetsPath() + "shaders/" + m_skyShaderName};
   m_skyProgram = createProgramFromFile(path + ".vert", path + ".frag");
 
   // Generate VBO
@@ -278,7 +267,6 @@ void OpenGLWindow::renderSkybox() {
 }
 
 void OpenGLWindow::paintUI() {
-
   // File browser for models
   static ImGui::FileBrowser fileDialogModel;
   fileDialogModel.SetTitle("Load 3D Model");
@@ -289,7 +277,7 @@ void OpenGLWindow::paintUI() {
   // File browser for textures
   static ImGui::FileBrowser fileDialogDiffuseMap;
   fileDialogDiffuseMap.SetTitle("Load Diffuse Map");
-  fileDialogDiffuseMap.SetTypeFilters({".jpg", ".png" });
+  fileDialogDiffuseMap.SetTypeFilters({".jpg", ".png"});
   fileDialogDiffuseMap.SetWindowSize(m_viewportWidth * 0.8f,
                                      m_viewportHeight * 0.8f);
 
@@ -316,10 +304,9 @@ void OpenGLWindow::paintUI() {
       widgetSize.y += 26;
     }
 
-    auto flagsDisplayText{ ImGuiWindowFlags_NoBackground 
-                        |   ImGuiWindowFlags_NoTitleBar | 
-                            ImGuiWindowFlags_NoInputs |
-                            ImGuiWindowFlags_NoDecoration};
+    auto flagsDisplayText{
+        ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoTitleBar |
+        ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_NoDecoration};
 
     ImGui::SetNextWindowPos(ImVec2(m_viewportWidth - widgetSize.x - 5, 5));
     ImGui::SetNextWindowSize(widgetSize);
@@ -345,59 +332,62 @@ void OpenGLWindow::paintUI() {
       if (loadNormalMap) fileDialogNormalMap.Open();
     }
 
-  // Aviso de gameover
-  if(houveColisao) {
-    
-    //Titulo do jogo
-    {
-      auto title_size{ImVec2(600, 200)};
-      auto title_position{ImVec2(0,0)};
+    // Aviso de gameover
 
-      ImGui::SetNextWindowPos(title_position);
-      ImGui::SetNextWindowSize(title_size);
-      ImGui::Begin(" ", nullptr,  flagsDisplayText);
-      ImGui::PushFont(m_font_title);
+  
 
-      ImGui::Text("The Tree Log Challenge!");
+    if (houveColisao) {
+      auto game_over_size{ImVec2(400, 200)};
+      // auto game_over_position{ImVec2((m_viewportWidth - game_over_size.x)
+      // / 2.0f,
+      //                       (m_viewportHeight - game_over_size.y) / 2.0f)};
+      auto game_over_position{ImVec2(0, 200)};
+
+      ImGui::SetNextWindowPos(game_over_position);
+      ImGui::SetNextWindowSize(game_over_size);
+      ImGui::Begin(" ", nullptr, flagsDisplayText);
+      ImGui::PushFont(m_font_game_over);
+
+      ImGui::Text("Fim de Jogo!");
+      ImGui::Text("   %d pts", pontos);
 
       ImGui::PopFont();
       ImGui::End();
+    } else {
+        // Titulo do jogo
+
+    auto title_size{ImVec2(600, 200)};
+    auto title_position{ImVec2(0, 0)};
+
+    ImGui::SetNextWindowPos(title_position);
+    ImGui::SetNextWindowSize(title_size);
+    ImGui::Begin(" ", nullptr, flagsDisplayText);
+    ImGui::PushFont(m_font_title);
+
+    ImGui::Text("The Tree Log Challenge!");
+
+    ImGui::PopFont();
+    ImGui::End();
+
+    if (acelerar) {
+      auto size{ImVec2(340, 180)};
+      auto position{ImVec2(0, 0)};
+
+      ImGui::SetNextWindowPos(position);
+      ImGui::SetNextWindowSize(size);
+      ImGui::Begin(" ", nullptr, flagsDisplayText);
+      ImGui::PushFont(m_font_message);
+
+      ImGui::Text("%d pontos!\nAcelerando 20%%", pontos);
+
+      ImGui::PopFont();
+      ImGui::End();
+      }
     }
 
-    auto game_over_size{ImVec2(400, 200)};
-    // auto game_over_position{ImVec2((m_viewportWidth - game_over_size.x) / 2.0f,
-    //                       (m_viewportHeight - game_over_size.y) / 2.0f)};
-    auto game_over_position{ImVec2(0, 200)};
-
-    ImGui::SetNextWindowPos(game_over_position);
-    ImGui::SetNextWindowSize(game_over_size);
-    ImGui::Begin(" ", nullptr,  flagsDisplayText);
-    ImGui::PushFont(m_font_game_over);
-
-    ImGui::Text("Fim de Jogo!");
-    ImGui::Text("   %d pts", pontos);
-
-    ImGui::PopFont();
-    ImGui::End();
-  }
-
-  //A cada cinco pontos acumulados ele exibe a mensagem de que esta acelerando
-  if(acelerar)
-  {
-    auto size{ImVec2(340, 180)};
-    auto position{ImVec2((m_viewportWidth - size.x) / 2.0f,
-                          (m_viewportHeight - size.y - 20))};
-
-    ImGui::SetNextWindowPos(position);
-    ImGui::SetNextWindowSize(size); 
-    ImGui::Begin(" ", nullptr,  flagsDisplayText);
-    ImGui::PushFont(m_font_message);
-
-    ImGui::Text("%d pontos!\nAcelerando 20%%", pontos);
-
-    ImGui::PopFont();
-    ImGui::End();
-  }
+    // A cada cinco pontos acumulados ele exibe a mensagem de que esta
+    // acelerando
+    
     // Slider will be stretched horizontally
     ImGui::PushItemWidth(widgetSize.x - 16);
     ImGui::SliderInt("", &m_trianglesToDraw, 0, m_model.getNumTriangles(),
@@ -588,7 +578,7 @@ void OpenGLWindow::resizeGL(int width, int height) {
 
   // m_trackBallModel.resizeViewport(width, height);
   // m_trackBallLight.resizeViewport(width, height);
-  
+
   m_camera.computeProjectionMatrix(width, height);
 }
 
@@ -605,40 +595,38 @@ void OpenGLWindow::terminateSkybox() {
   glDeleteVertexArrays(1, &m_skyVAO);
 }
 
-void OpenGLWindow::translateModel(float speed){
-  
+void OpenGLWindow::translateModel(float speed) {
   glm::vec3 m_forward{glm::vec3(0.0f, 0.0f, 1.0f)};
 
-  //Vetor unitario da direcao X velocidade X fator de aceleracao
+  // Vetor unitario da direcao X velocidade X fator de aceleracao
   m_forward = m_forward * speed * m_logSpeedFactor;
 
   m_modelMatrix = glm::translate(m_modelMatrix, m_forward);
 }
 
-//Leva o modelo de volta a posicao inicial
-void OpenGLWindow::resetModelPosition(){
-  m_modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, -1 * m_logSpeedFactor));
+// Leva o modelo de volta a posicao inicial
+void OpenGLWindow::resetModelPosition() {
+  m_modelMatrix =
+      glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, -1 * m_logSpeedFactor));
 }
 
 void OpenGLWindow::update() {
-  
   float deltaTime{static_cast<float>(getDeltaTime())};
 
   elapsedTime += deltaTime;
-  
-  if(isJumping){
-    if(!avisoPulandoDado){
+
+  if (isJumping) {
+    if (!avisoPulandoDado) {
       printf("esta pulando\n");
       avisoPulandoDado = true;
       avisoEncerrouPuloDado = false;
     }
-    m_camera.jump(m_jumpSpeed * deltaTime * m_jumpSpeedFactor);  
+    m_camera.jump(m_jumpSpeed * deltaTime * m_jumpSpeedFactor);
 
-    if(m_camera.m_at.y > 0.49f){
+    if (m_camera.m_at.y > 0.49f) {
       m_jumpSpeed -= 3 * deltaTime;
-    }
-    else {
-      if(!avisoEncerrouPuloDado){
+    } else {
+      if (!avisoEncerrouPuloDado) {
         printf("encerrou o pulo\n");
         avisoEncerrouPuloDado = true;
         avisoPulandoDado = false;
@@ -649,29 +637,28 @@ void OpenGLWindow::update() {
     }
   }
 
-  if(getZPos(m_modelMatrix) < 2.5f){
+  if (getZPos(m_modelMatrix) < 2.5f) {
     translateModel(m_LogSpeed * deltaTime);
-  }
-  else{
-    if(!houveColisao){
+  } else {
+    if (!houveColisao) {
       pontos++;
     }
     resetModelPosition();
     elapsedTime = 0.0f;
   }
 
-  if(pontos > 0 && pontos % 5 == 0 && !acelerar){
-    //incrementa a velocidade em 20% a cada 5 troncos pulados
+  if (pontos > 0 && pontos % 5 == 0 && !acelerar) {
+    // incrementa a velocidade em 20% a cada 5 troncos pulados
     m_logSpeedFactor += 0.2f;
     m_jumpSpeedFactor += 0.2f;
 
     acelerar = true;
   }
 
-  if(acelerar){
+  if (acelerar) {
     elapsedMsgTimer += deltaTime;
 
-    if(elapsedMsgTimer >= displayMsgTimer){
+    if (elapsedMsgTimer >= displayMsgTimer) {
       acelerar = false;
       elapsedMsgTimer = 0.0f;
     }
@@ -679,30 +666,30 @@ void OpenGLWindow::update() {
 
   checkCollisions();
 
-  //Update LookAt camera
+  // Update LookAt camera
   m_camera.dolly(m_dollySpeed * deltaTime);
   m_camera.truck(m_truckSpeed * deltaTime);
   m_camera.pan(m_panSpeed * deltaTime);
 
-  if(houveColisao){
+  if (houveColisao) {
     restartTimer -= deltaTime;
-    if(restartTimer <= 0){
+    if (restartTimer <= 0) {
       restart();
     }
   }
 }
 
 void OpenGLWindow::checkCollisions() {
-  float zModel =getZPos(m_modelMatrix);
-  //houve colisao: o tronco da arvore esta proximo e o personagem nao esta pulando
-  houveColisao = houveColisao || ((zModel >= +2.1f && zModel <= 2.5f) && !isJumping);
+  float zModel = getZPos(m_modelMatrix);
+  // houve colisao: o tronco da arvore esta proximo e o personagem nao esta
+  // pulando
+  houveColisao =
+      houveColisao || ((zModel >= +2.1f && zModel <= 2.5f) && !isJumping);
 }
 
-float OpenGLWindow::getZPos(glm::mat4 matrix){
-  return matrix[3][2];
-}
+float OpenGLWindow::getZPos(glm::mat4 matrix) { return matrix[3][2]; }
 
-void OpenGLWindow::restart(){
+void OpenGLWindow::restart() {
   pontos = 0;
   houveColisao = false;
   resetModelPosition();
